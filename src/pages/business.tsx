b54f304304228bd76d56c0cba2f909558a7a0240
JP Erasmus <jperasmus11@gmail.com>
2019-12-21 21:34:56 +0200

    feat: add business page content

--- src/pages/business.tsx
+++ src/pages/business.tsx
@@ -1,42 +1,73 @@
 import React from 'react'
+import { css } from '@emotion/core'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import PageBanner from '../components/PageBanner'
 import { Section, SectionContainer, SectionTitle } from '../components/Section'
 import TestimonialsSlider from '../components/TestimonialsSlider'
 import ContactUsSection from '../components/ContactUsSection'
+import ImageRevealSection from '../components/ImageRevealSection'
+import IconCopyBlocks from '../components/IconCopyBlocks'
 
-function BusinessPage() {
+function BusinessPage({ data }) {
+  const pageContent = data.flamelinkBusinessPersonaPageContent
+
+  const {
+    pageTitle,
+    overviewSection,
+    featuresSection,
+    caseStudiesSection,
+    testimonialsSection
+  } = pageContent
+
   return (
     <Layout>
       <SEO
         keywords={['flamelink', 'business', 'enterprise']}
-        title="Business"
+        title={pageTitle}
       />
       <main
         css={css`
           scroll-snap-type: x proximity;
         `}
       >
-        <PageBanner title="Business" />
-        <Section className="bg-white">
+        <PageBanner title={pageTitle} />
+        <ImageRevealSection
+          bg="white"
+          heading={overviewSection.title}
+          content={overviewSection.excerpt}
+          imagePosition={overviewSection.imagePosition}
+          imageYOverlap={overviewSection.imageYOverlap}
+          fluidImage={get(overviewSection, 'image[0].localFile')}
+        />
+        <Section className="bg-gray-100">
           <SectionContainer>
-            <SectionTitle>Quick and Secure</SectionTitle>
+            <p className="text-center max-w-4xl mb-15">
+              {featuresSection.excerpt}
+            </p>
+            <IconCopyBlocks
+              className="-mb-10"
+              blocks={get(featuresSection, 'features', []).map(feature => ({
+                title: feature.title,
+                iconUrl: feature.icon[0].url
+              }))}
+            />
           </SectionContainer>
         </Section>
-        <Section className="bg-gray-100">
-          <SectionContainer></SectionContainer>
-        </Section>
         <Section className="bg-white">
           <SectionContainer>
-            <SectionTitle>Case Studies</SectionTitle>
+            <SectionTitle>{caseStudiesSection.title}</SectionTitle>
           </SectionContainer>
         </Section>
 
         <Section className="bg-gray-100">
           <SectionContainer>
-            <SectionTitle>What our clients say</SectionTitle>
-            <TestimonialsSlider testimonials={[]} />
+            <SectionTitle>{testimonialsSection.title}</SectionTitle>
+            <TestimonialsSlider
+              testimonials={get(testimonialsSection, 'testimonials', [])}
+            />
           </SectionContainer>
         </Section>
         <ContactUsSection />
@@ -46,3 +77,109 @@ function BusinessPage() {
 }
 
 export default BusinessPage
+
+export const query = graphql`
+  query BusinessPersonaPageQuery {
+    flamelinkBusinessPersonaPageContent {
+      pageTitle
+      overviewSection {
+        imageYOverlap
+        imagePosition
+        excerpt
+        title
+        image {
+          localFile {
+            childImageSharp {
+              fluid {
+                base64
+                tracedSVG
+                aspectRatio
+                src
+                srcSet
+                srcWebp
+                srcSetWebp
+                sizes
+                originalImg
+                originalName
+                presentationWidth
+                presentationHeight
+              }
+            }
+          }
+        }
+      }
+      featuresSection {
+        excerpt
+        features {
+          title
+          icon {
+            url
+          }
+        }
+      }
+      caseStudiesSection {
+        title
+        caseStudies {
+          title
+          slug
+          backgroundImage {
+            localFile {
+              childImageSharp {
+                fluid {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  sizes
+                  originalImg
+                  originalName
+                  presentationWidth
+                  presentationHeight
+                }
+              }
+            }
+          }
+          excerpt
+          logo {
+            localFile {
+              childImageSharp {
+                fixed {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  width
+                  height
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  originalName
+                }
+              }
+            }
+          }
+        }
+      }
+      testimonialsSection {
+        title
+        testimonials {
+          avatar {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 120) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          name
+          jobTitle
+          quote
+        }
+      }
+    }
+  }
+`
