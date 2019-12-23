b92f1ebbaaa6092c3171c9ceb173b4d746b3e60e
JP Erasmus <jperasmus11@gmail.com>
2019-12-23 14:42:53 +0200

    feat: contact us form

---
+++ src/hooks/contact-us-form.tsx
@@ -0,0 +1,57 @@
+import { unstable_useFormState as useFormState } from 'reakit/Form'
+import { string, object } from 'yup'
+import set from 'lodash/set'
+import { getFirebaseApp } from '../utils/firebase'
+
+const schema = object().shape({
+  name: string().required('Please enter your name'),
+  email: string()
+    .email()
+    .required('Please enter a valid email'),
+  message: string().required('Please enter a message')
+})
+
+export const useContactUsForm = () => {
+  return useFormState({
+    resetOnUnmount: true,
+    resetOnSubmitSucceed: false,
+    values: { name: '', email: '', message: '' },
+    onValidate: values =>
+      schema.validate(values, { abortEarly: false }).then(
+        () => {
+          // success no-op
+        },
+        error => {
+          if (error.inner.length) {
+            throw error.inner.reduce(
+              (acc: Object, curr: any) => set(acc, curr.path, curr.message),
+              {}
+            )
+          }
+        }
+      ),
+    onSubmit: async payload => {
+      try {
+        const firebaseApp = await getFirebaseApp()
+
+        await firebaseApp
+          .firestore()
+          .collection('contactUs')
+          .add({
+            ...payload,
+            created: Date.now()
+          })
+
+        console.log(
+          '[FLAMELINK]: Successfully submitted contact us form',
+          payload
+        )
+      } catch (error) {
+        console.error(error)
+
+        // eslint-disable-next-line no-throw-literal
+        throw { name: error.message }
+      }
+    }
+  })
+}
