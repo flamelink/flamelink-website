686302ebed1e4f0c8bad73b432ef9f7cbcedbd63
JP Erasmus <jperasmus11@gmail.com>
2020-01-21 13:57:18 +0200

    fix: SSR for meta tags

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import { Provider } from 'reakit'
 import { ThemeProvider } from 'emotion-theming'
-import { HelmetProvider } from 'react-helmet-async'
 import { useTheme } from '../hooks/theme'
 
 const AppProviders: React.FC = ({ children }) => {
   const theme = useTheme()
 
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={theme}>
-        <Provider>
-          {typeof children === 'function' ? children(theme) : children}
-        </Provider>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <Provider>
+        {typeof children === 'function' ? children(theme) : children}
+      </Provider>
+    </ThemeProvider>
   )
 }
 
