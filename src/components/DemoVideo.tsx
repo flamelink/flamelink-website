83fefc2efe2aa381f5687dcc81f6419d63d05a08
Coenraad Fick <coenraad@responsive.co.za>
2020-09-07 16:39:16 +0200

    GTM Final Updates
    - Added the GTM events to the carousel clicks
    - Updated the Demo Video iframe source, as requested

--- src/components/DemoVideo.tsx
+++ src/components/DemoVideo.tsx
@@ -10,12 +10,12 @@ const DemoVideo = ({ visible }: { visible: boolean }) => {
       className="w-full h-full"
       width="100%"
       height="100%"
-      frameBorder="0"
       allowFullScreen
+      frameBorder="0"
+      src="https://www.youtube.com/embed/8Cw5ktNADBQ"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       title="Player for Flamelink.io a Firebase CMS"
-      src="https://www.youtube-nocookie.com/embed/8Cw5ktNADBQ?=controls=0&amp;rel=0&amp;showinfo=0&amp;autoplay=1&amp;iv_load_policy=3&amp;cc_load_policy=1&amp;cc_lang_pref=en&amp;wmode=transparent&amp;modestbranding=1&amp;disablekb=0&amp;origin=https%3A%2F%2Fflamelink.io&amp;enablejsapi=0&amp;widgetid=4"
-    />
+    ></iframe>
   )
 }
 
